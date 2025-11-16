ed72d685880f607226dbe55e68dec78c4db1ff49
samuelHurh <66130364+samuelHurh@users.noreply.github.com>
2025-11-15 20:19:36 -0800

    added rh gestures but they are not implemented

---
+++ UnityProject/Assets/ProjectileSpell.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ProjectileSpell : MonoBehaviour
+{
+    // // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+    public void Point()
+    {
+        Debug.Log("point registered");
+    }
+
+    public void Prime()
+    {
+        Debug.Log("Prime registered");
+    }
+    
+    public void Reload()
+    {
+        Debug.Log("reload registered");
+    }
+}
